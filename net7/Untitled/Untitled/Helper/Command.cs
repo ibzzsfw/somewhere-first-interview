573f984c20ed513fa9bdd1521e5db54341ac351f
ibzzsfw <tap.pt33@gmail.com>
2023-11-14 01:06:11 +0700

    [NET7] feat: create client

--- net7/Untitled/Untitled/Helper/Command.cs
+++ net7/Untitled/Untitled/Helper/Command.cs
@@ -3,16 +3,27 @@ using Untitled.Controllers;
 
 namespace Untitled.Helper;
 
-public static class Command
+public class Command
 {
-    public static void CreateRoom(string command, RoomController controller)
+    private readonly RoomController _roomController;
+    private readonly BookingController _bookingController;
+    private readonly ReportController _reportController;
+
+    public Command(IServiceProvider serviceProvider)
+    {
+        _roomController = ServiceProviderWrapper.GetService<RoomController>(serviceProvider);
+        _bookingController = ServiceProviderWrapper.GetService<BookingController>(serviceProvider);
+        _reportController = ServiceProviderWrapper.GetService<ReportController>(serviceProvider);
+    }
+
+    public void CreateRoom(string command)
     {
         var name = CommandRegex.CreateRoom.Match(command).Groups["name"].Value;
 
-        controller.Create(name);
+        _roomController.Create(name);
     }
 
-    public static void BookingByRoomId(string command, BookingController controller)
+    public void BookingByRoomId(string command)
     {
         var match = CommandRegex.BookingByRoomId.Match(command);
 
@@ -20,10 +31,10 @@ public static class Command
         var checkIn = DateHelper.ToDateTime(match.Groups["checkIn"].Value);
         var checkOut = DateHelper.ToDateTime(match.Groups["checkOut"].Value);
 
-        controller.Book(roomId, checkIn, checkOut);
+        _bookingController.Book(roomId, checkIn, checkOut);
     }
 
-    public static void BookingByRoomName(string command, BookingController controller)
+    public void BookingByRoomName(string command)
     {
         var match = CommandRegex.BookingByRoomName.Match(command);
 
@@ -31,23 +42,23 @@ public static class Command
         var checkIn = DateHelper.ToDateTime(match.Groups["checkIn"].Value);
         var checkOut = DateHelper.ToDateTime(match.Groups["checkOut"].Value);
 
-        controller.Book(roomName, checkIn, checkOut);
+        _bookingController.Book(roomName, checkIn, checkOut);
     }
 
-    public static void CancelBooking(string command, BookingController controller)
+    public void CancelBooking(string command)
     {
         var bookingId = int.Parse(CommandRegex.CancelBooking.Match(command).Groups["bookingId"].Value);
 
-        controller.Cancel(bookingId);
+        _bookingController.Cancel(bookingId);
     }
 
-    public static void Report(ReportController controller)
+    public void Report()
     {
-        controller.Report();
+        _reportController.Report();
     }
 
-    public static void ListRooms(RoomController controller)
+    public void ListRooms()
     {
-        controller.List();
+        _roomController.List();
     }
 }
--- net7/Untitled/Untitled/Program.cs
+++ net7/Untitled/Untitled/Program.cs
@@ -1,23 +1,19 @@
 ﻿using System.Text.RegularExpressions;
 using Untitled;
 using Untitled.Constant;
-using Untitled.Controllers;
 using Untitled.Helper;
 
 var serviceProvider = ServiceProviderWrapper.GetServiceProvider();
-
-var roomController = ServiceProviderWrapper.GetService<RoomController>(serviceProvider);
-var bookingController = ServiceProviderWrapper.GetService<BookingController>(serviceProvider);
-var reportController = ServiceProviderWrapper.GetService<ReportController>(serviceProvider);
+var client = new Command(serviceProvider);
 
 var commands = new Dictionary<Regex, Action<string>>
 {
-    { CommandRegex.CreateRoom, s => Command.CreateRoom(s, roomController) },
-    { CommandRegex.BookingByRoomId, s => Command.BookingByRoomId(s, bookingController) },
-    { CommandRegex.BookingByRoomName, s => Command.BookingByRoomName(s, bookingController) },
-    { CommandRegex.CancelBooking, s => Command.CancelBooking(s, bookingController) },
-    { CommandRegex.Report, s => Command.Report(reportController) },
-    { CommandRegex.ListRooms, s => Command.ListRooms(roomController) },
+    { CommandRegex.CreateRoom, s => client.CreateRoom(s) },
+    { CommandRegex.BookingByRoomId, s => client.BookingByRoomId(s) },
+    { CommandRegex.BookingByRoomName, s => client.BookingByRoomName(s) },
+    { CommandRegex.CancelBooking, s => client.CancelBooking(s) },
+    { CommandRegex.Report, s => client.Report() },
+    { CommandRegex.ListRooms, s => client.ListRooms() },
     {
         CommandRegex.Exit, _ =>
         {
